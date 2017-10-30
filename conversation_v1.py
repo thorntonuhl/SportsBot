f2c9ddfd4faab41d3ebf484fd336c183a4c54f2e
danielrthomas <danielrthomas1@gmail.com>
2017-10-30 18:29:31 -0500

    Code

--- conversation_v1.py
+++ conversation_v1.py
@@ -1,6 +1,8 @@
 import json
 from watson_developer_cloud import ConversationV1
-from news_api import *
+#from news_api import *
+from ay_news_api import *
+import pdb
 
 #########################
 # message
@@ -13,29 +15,36 @@ conversation = ConversationV1(username=str(credentials['username']), password=st
 
 workspace_id = '8576b6f8-005a-4eae-9859-52c798ab7214'
 
-article = {}
+story = {"body" : ''}
 watson_response = {}
 
 typed = ''
 response_message = ''
-initial_message = "Hi, I'm Billy, The Cheeky Chatbot! I'll look things up for you!\n"
+initial_message = "Hi, I'm Billy, The Cheeky Chatbot! I'll look things up for you!\n\n\n"
 
 def conv_att():
     typed = ''
     response_message = ''
-    while typed != 'QUIT':
+    while typed != 'QUIT':              
         if response_message != '':
             typed = raw_input(response_message)
         else:
             typed = raw_input(initial_message)
         print "\n"
-        watson_response = conversation.message(workspace_id=workspace_id, message_input={'text': typed})
-        response_message = watson_response['output']['text'][0]
-        try:
-            lookup_str = watson_response["entities"][0]["value"]
-        except:
-            lookup_str = typed
-        article, res = lookup(lookup_str)
-        response_message += "\n\n" + res + "\n"
+        if typed.lower() == "more":
+            print story.body + '\n\n'
+            typed = raw_input()
+        else:
+            watson_response = conversation.message(workspace_id=workspace_id, message_input={'text': typed})
+            response_message = ''#watson_response['output']['text'][0]
+            #print watson_response
+            try:
+                lookup_str = watson_response["entities"][0]["value"]
+                subject = watson_response["intents"][0]["value"]
+            except:
+                lookup_str = typed
+                subject = ''
+            story, res = ay_lookup(lookup_str, subject)
+            response_message += res + "\n\n\n"
 
 conv_att()
