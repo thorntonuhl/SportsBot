81d7e44d7fe1d76271b7f02d292dfad19c3a7532
danielrthomas <danielrthomas1@gmail.com>
2017-11-06 17:02:20 -0600

    Better files and complete run

--- ay_news_api.py
+++ ay_news_api.py
@@ -3,7 +3,7 @@ from aylien_news_api.rest import ApiException
 import re
 import pdb
 
-def ay_lookup(keyword, subject = '',entities = []):
+def ay_lookup(keyword, subject = '',entities = [],timeframe = '0,2'):
     # Configure API key authorization: app_id
     aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-ID'] = 'e6ddf398'
     # Configure API key authorization: app_key
@@ -17,12 +17,19 @@ def ay_lookup(keyword, subject = '',entities = []):
       'body': keyword,
       'sort_by': 'relevance',
       'language': ['en'],
-      'published_at_start': 'NOW-2DAYS',
-      'published_at_end': 'NOW',
       'entities_body_text' : entities
     }
     if subject != '':
         opts['text'] = subject
+    first = timeframe.split(',')[0]
+    second = timeframe.split(',')[1]
+    if first == '0':
+        first = 'NOW'
+    else:
+        first = 'NOW-'+first+'DAYS'
+    second = 'NOW-'+second+'DAYS'
+    opts['published_at_start'] = second
+    opts['published_at_end'] = first
     try:
         api_response = api_instance.list_stories(**opts)
         api_response.stories
@@ -30,41 +37,42 @@ def ay_lookup(keyword, subject = '',entities = []):
             story = api_response.stories[i]
             if "Sign up for one of our email newsletters." not in story.body:
                 try:
-                    body_clauses = story.body.replace("\n"," ").split('. ')
+                    body_clauses = clean_st(story.body.replace("\n"," ")).split('.')
+                    while '' in body_clauses:
+                        body_clauses.remove('')
                 except:
                     body_clauses = []
-                lst = body_clauses
                 try:
                     summary = story.summary['sentences']
                 except:
                     summary = []
+                summarycounter = 0
+                bodycounter = 0
                 st = ''
-                counter1 = 0
-                counter2 = 0
-                while counter2 < len(body_clauses) and st == '':
+                body_clauses = fix_clauses(body_clauses)
+                while '' in body_clauses:
+                    body_clauses.remove('')
+                while bodycounter < len(body_clauses) and st == '':
                     try:
-                        st = summary[counter1]
-                        intitial = counter1
-                        counter1 += 1
-                        initial = counter1
+                        st = summary[summarycounter]
+                        initial = summarycounter
+                        summarycounter += 1
+                        lst = body_clauses
                     except:
-                        st = body_clauses[counter2]+"."
-                        initial = counter2
-                        counter2 += 1
-                    top = initial + 1
-                    st = st.replace('\n',' ')
-                    if len(st) > 40:
-                        st = st.split('. ')[0]+"."
-                    if initial != counter2:
+                        st = body_clauses[bodycounter]+"."
+                        initial = bodycounter
+                        bodycounter += 1
                         lst = summary
+                    top = initial + 1
+                    st = clean_st(st)
                     while bad_string(st) and top < len(lst):
                         if lst[initial:top] != []:
-                            st = lst[initial:top].join('. ') + "."
+                            st = lst[initial:top].join('.') + '.'
                         top += 1
                     if 'photo' in st.lower() or 'file' in st.lower() or st[0] == st[0].lower() or '(' in st or ')' in st:
                         st = ''
-                    st = st.replace('..','.')
-                if len(st) > 10:
+                    st = clean_st(st)
+                if len(st) > 10 and bad_string(st) != True:
                     return story, st
     except ApiException as e:
         print("Exception when calling DefaultApi->list_stories: %s\n" % e)
@@ -72,16 +80,59 @@ def ay_lookup(keyword, subject = '',entities = []):
 
 def bad_string(st):
     try:
-        if (re.match("( ...\.)", st[len(st)-5:]) != None or
-            re.match("(^*.\.)", st[len(st)-10:]) != None or
-            re.match("( .\.)", st[len(st)-3:]) != None or
-            re.match("( ..\.)", st[len(st)-4:]) != None):
+        if regex_check(clean_st(st)) == False:
             return True
-        elif len(st) < 10:
-            return True
-        elif "vs." in st[len(st)-4:]:
+        elif len(st) < 15:
             return True
         else:
             return False
     except:
         return True
+
+def regex_check(st):
+    if st[-1] == '.':
+        st = st[:-1]
+    t = ' '+st
+    if re.match("(?! .)", t[len(t)-2:]) == None:
+        return False
+    if re.match("(?! ..)", t[len(t)-3:]) == None:
+        return False
+    if re.match("(?! ...)", t[len(t)-4:]) == None:
+        return False
+    return True
+
+
+def clean_st(st):
+    if st == '':
+        return st
+    try:
+        if 'About ' in st[:6]:
+            st = st[6:]
+    except:
+        pass
+    st = st.replace('\n',' ')
+    while st != '' and st[0] == ' ':
+        st = st[1:]
+    while st != '' and st[-1] == ' ':
+        st = st[:len(st)-1]
+    while '  ' in st:
+        st = st.replace('  ',' ')
+    while '..' in st:
+        st = st.replace('..','.')
+    return st
+
+def fix_clauses(clauses):
+    clauses[-1] = clauses[-1][:-1]
+    if len(clauses) < 2:
+        return clauses
+    else:
+        current = 0
+        extra = current + 1
+        while extra < len(clauses):
+            if len(clauses[current]) < 20 or len(clauses[extra]) < 20 or not bad_string(clauses[current]):
+                clauses[current] = clauses[current] + '. ' + clauses[extra]
+                clauses.remove(clauses[extra])
+            else:
+                current += 1
+                extra = current + 1
+        return clauses
--- conversation_v1.py
+++ conversation_v1.py
@@ -1,12 +1,9 @@
 import json
 from watson_developer_cloud import ConversationV1
-#from news_api import *
 from ay_news_api import *
-import pdb
 
-#########################
-# message
-#########################
+from flask import Flask,render_template,request
+app = Flask(__name__)
 
 with open('credentials.json') as js:
     credentials = json.load(js)
@@ -18,44 +15,50 @@ workspace_id = '4821b731-49db-4497-9376-5fc787771924'
 story = {"body" : ''}
 watson_response = {}
 
-typed = ''
-response_message = ''
-initial_message = "Hi, I'm Billy, The Cheeky Chatbot! I'll look things up for you!\n\n\n"
 
-print '\n\n'
+@app.route('/')
+def input_display():
+    input_text = request.args.get('input')
 
-def conv_att():
-    typed = ''
-    response_message = ''
-    while typed.lower() != 'quit':
-        if response_message == '':
-            typed = raw_input(initial_message)
+    if input_text == None or input_text == '':
+        return render_template('sports_bot.html', name='I\'m the sports bot. How can I help?')
+    
+    else:
+        output = conv_att(input_text)
+        return render_template('sports_bot.html', name=output)
+
+res = ''
+subject = ''
+
+def conv_att(input_text):
+        if "subject" in input_text.lower():
+            try:
+                return subject
+            except:
+                return "Subject Unavailable."
+        elif "more" in input_text.lower():
+            try:
+                return story.body
+            except:
+                return "Story Unavailable."
+        elif "source" in input_text.lower():
+            try:
+                return story.source.name
+            except:
+                return "Source Unavailable."
+        elif "author" in input_text.lower():
+            try:
+                return story.author.name
+            except:
+                return "Author Unavailable."
+        elif "description" in input_text.lower():
+            if res != '':
+                return res
+            else:
+                return "Description Unavailable."
         else:
-            typed = raw_input(response_message)
-            
-        print "\n"
-        
-        if "more" in typed.lower():
-            response_message = story.body
-        elif "source" in typed.lower():
-            try:
-                response_message = story.source.name
-            except:
-                response_message = "Source Unavailable."
-        elif "author" in typed.lower():
-            try:
-                response_message = story.author.name
-            except:
-                response_message = "Author Unavailable."
-        elif "description" in typed.lower():
-            try:
-                response_message = res
-            except:
-                response_message = "Description Unavailable."
-        else:
-            watson_response = conversation.message(workspace_id=workspace_id, message_input={'text': typed})
-            response_message = ''#watson_response['output']['text'][0]
-            #print watson_response
+            watson_response = conversation.message(workspace_id=workspace_id, message_input={'text': input_text})
+            response_message = ''
             entities = []
             try:
                 lookup_str = watson_response["entities"][0]["value"]
@@ -66,10 +69,15 @@ def conv_att():
                 except:
                     subject = ''
             except:
-                lookup_str = typed
+                lookup_str = input_text
                 subject = ''
-            story, res = ay_lookup(lookup_str, subject, entities)
-            response_message += res
-        response_message += "\n\n\n"
-
-conv_att()
+            response = None
+            counter = 2
+            while response == None:
+                response = ay_lookup(lookup_str, subject, entities, '0,'+str(counter))
+                counter += 2
+            story = response[0]
+            res = response[1]
+        return res
+        
+app.run()
